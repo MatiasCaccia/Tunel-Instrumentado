31fa99b27ec7d7ec410f8df045004bdf7038c492
MatiasCaccia <matiascacciag@gmail.com>
2023-07-18 14:17:27 -0300

    Add files 2

---
+++ interfaz.py
@@ -0,0 +1,108 @@
+from PyQt5 import QtCore, QtGui, QtWidgets
+
+class Ui_MainWindow(object):
+    def setupUi(self, MainWindow):
+        # Pantalla Principal
+        MainWindow.setObjectName("MainWindow")
+        MainWindow.resize(1000, 600)
+        self.centralwidget = QtWidgets.QWidget(MainWindow)
+        self.centralwidget.setObjectName("centralwidget")
+        self.groupBox = QtWidgets.QGroupBox(self.centralwidget)
+        self.groupBox.setGeometry(QtCore.QRect(0, 0, 990, 590))
+        self.groupBox.setObjectName("groupBox")
+
+        # Sensor 1
+        self.lcdPresion1 = QtWidgets.QLCDNumber(10, self.groupBox)
+        self.lcdPresion1.setGeometry(QtCore.QRect(200, 20, 300, 41))
+        self.lcdPresion1.setObjectName("lcdPresion1")
+        self.lcdTemperatura1 = QtWidgets.QLCDNumber(10, self.groupBox)
+        self.lcdTemperatura1.setGeometry(QtCore.QRect(200, 80, 300, 41))
+        self.lcdTemperatura1.setObjectName("lcdPTemperatura1")
+        '''
+        self.lcdNumberCur = QtWidgets.QLCDNumber(self.groupBox)
+        
+        self.lcdNumberCur = QtWidgets.QLCDNumber(self.groupBox)
+        '''
+        
+        # Sensor 2 presión
+        self.lcdPresion2 = QtWidgets.QLCDNumber(10, self.groupBox)
+        self.lcdPresion2.setGeometry(QtCore.QRect(200, 200, 300, 41))
+        self.lcdPresion2.setObjectName("lcdPresion2")
+        self.lcdTemperatura2 = QtWidgets.QLCDNumber(10, self.groupBox)
+        self.lcdTemperatura2.setGeometry(QtCore.QRect(200, 260, 300, 41))
+        self.lcdTemperatura2.setObjectName("lcdPTemperatura2")
+        #self.lcdNumberCur2 = QtWidgets.QLCDNumber(self.groupBox)
+
+        # Timestamp
+        self.lcdTimestamp = QtWidgets.QLCDNumber(self.groupBox)
+        self.lcdTimestamp.setGeometry(QtCore.QRect(800, 310, 151, 41))
+        self.lcdTimestamp.setObjectName("lcdTimestamp")
+
+        # Texto a la izquierda del display
+        self.label = QtWidgets.QLabel(self.groupBox)
+        self.label.setGeometry(QtCore.QRect(20, 50, 150, 50))
+        self.label.setObjectName("label")
+
+        # Texto a la izquierda del display para Sensor 1
+        self.labelPresion1 = QtWidgets.QLabel(self.groupBox)
+        self.labelPresion1.setGeometry(QtCore.QRect(15, 10, 150, 50))
+        self.labelPresion1.setObjectName("labelPresion1")
+        self.labelTemperatura1 = QtWidgets.QLabel(self.groupBox)
+        self.labelTemperatura1.setGeometry(QtCore.QRect(15, 75, 160, 50))
+        self.labelTemperatura1.setObjectName("labelTemperatura1")
+
+        # Texto a la izquierda del display para Sensor 2
+        self.labelPresion2 = QtWidgets.QLabel(self.groupBox)
+        self.labelPresion2.setGeometry(QtCore.QRect(15, 195, 150, 50))
+        self.labelPresion2.setObjectName("labelPresion2")
+        self.labelTemperatura2 = QtWidgets.QLabel(self.groupBox)
+        self.labelTemperatura2.setGeometry(QtCore.QRect(15, 260, 160, 50))
+        self.labelTemperatura2.setObjectName("labelTemperatura2")
+
+        self.labelMin = QtWidgets.QLabel(self.groupBox)
+        self.labelMin.setGeometry(QtCore.QRect(20, 95, 150, 50))
+        self.labelMin.setObjectName("labelMin")
+        
+        # Boton de envio de sincronia
+        self.pushButton = QtWidgets.QPushButton("pushButton", self.groupBox) 
+        self.pushButton.setGeometry(QtCore.QRect(100, 500, 100, 50))
+        self.pushButton.setObjectName("pushButton")
+
+        # Boton de envio de STOP
+        self.pushButton_stop = QtWidgets.QPushButton("pushButton_stop", self.groupBox) 
+        self.pushButton_stop.setGeometry(QtCore.QRect(750, 500, 100, 50))
+        self.pushButton_stop.setObjectName("pushButton_stop")
+
+        # Entrada de texto para la presión de referencia
+        self.ei = QtWidgets.QLineEdit(self.groupBox)
+        self.ei.setGeometry(QtCore.QRect(600, 20, 300, 41))
+        self.ei.setObjectName("ValorReferencia")
+
+        MainWindow.setCentralWidget(self.centralwidget)
+        self.statusbar = QtWidgets.QStatusBar(MainWindow)
+        self.statusbar.setObjectName("statusbar")
+        MainWindow.setStatusBar(self.statusbar)
+
+        self.retranslateUi(MainWindow)
+        QtCore.QMetaObject.connectSlotsByName(MainWindow)
+
+    def retranslateUi(self, MainWindow):
+        _translate = QtCore.QCoreApplication.translate
+        MainWindow.setWindowTitle(_translate("MainWindow", "Calibración BMP280"))
+        #self.groupBox.setTitle(_translate("MainWindow", "Datos Tiempo Real"))
+        self.labelPresion1.setText(_translate("MainWindow", "Presion Sensor 1 [Pa]"))
+        self.labelTemperatura1.setText(_translate("MainWindow", "Temperatura Sensor 1 [°C]"))
+        self.labelPresion2.setText(_translate("MainWindow", "Presion Sensor 2 [Pa]"))
+        self.labelTemperatura2.setText(_translate("MainWindow", "Temperatura Sensor 2 [°C]"))
+        self.pushButton.setText(_translate("MainWindow", "Comenzar"))
+        self.pushButton_stop.setText(_translate("MainWindow", "STOP"))
+        self.ei.setText(_translate("MainWundow", ""))
+
+if __name__ == "__main__":
+    import sys
+    app = QtWidgets.QApplication(sys.argv)
+    MainWindow = QtWidgets.QMainWindow()
+    ui = Ui_MainWindow()
+    ui.setupUi(MainWindow)
+    MainWindow.show()
+    sys.exit(app.exec_())
--- main.py
+++ main.py
@@ -1,3 +1,99 @@
-import numpy as np
+import sys
+import paho.mqtt.client as mqtt
+import csv
+from PyQt5.QtWidgets import QApplication, QMainWindow 
+from PyQt5.QtCore import QTimer
+import socket
 
-a = np.sqrt(2)
+from interfaz import Ui_MainWindow
+
+class Form(QMainWindow):
+    def __init__(self):
+        super().__init__()
+
+        self.ui = Ui_MainWindow()
+        self.ui.setupUi(self)
+
+        #self.i = 0 
+        #self.voltageMin = 180 
+        #self.voltageMax = 180
+        #self.ui.lcdNumberCur.display(self.i)
+
+        self.ui.pushButton.clicked.connect(self.sendSincro) #Asociar la funcion enviar epoch para sincronizar
+        # self.ui.pushButton.clicked.connect(self.on_message) #Asociar la funcion enviar epoch para sincronizar
+        self.ui.ei.text()
+        self.ui.pushButton_stop.clicked.connect(self.sendStop) #Asociar la funcion enviar cero para frenar
+        
+        self.ui.lcdPresion1.display(128)
+        self.ui.lcdTemperatura1.display(64)
+        self.ui.lcdPresion2.display(256)
+        self.ui.lcdTemperatura2.display(32)
+        self.show()
+        
+        # Configurar broker MQTT
+        self.broker_address = socket.gethostbyname(socket.gethostname()) #Obtener el IP de la maquina = IP del broker
+        self.topic = "Sincro" #Tópico donde se publica el epoch de sincronismo
+        self.subscribe_topic_1 = "Sensor" #Tópico al que se suscribe con información del nodo 1
+        self.subscribe_topic_2 = "Sensor2" #Tópico al que se suscribe con información del nodo 1
+        # MQTT client setup
+        self.client = mqtt.Client()
+        self.client.on_message = self.on_message #Ejecución de la funcion "on_message" cuando hay una lectura
+        self.client.on_connect = self.on_connect #Conexión al Ejecución de la funcion "on_message" cuando hay una lectura
+        self.client.connect(self.broker_address) #Conexión al Broker
+        self.client.subscribe(self.subscribe_topic_1) #Suscripción al tópico Sensor
+        self.client.subscribe(self.subscribe_topic_2) #Suscripción al tópico Sensor2
+        self.client.loop_start()
+        # Archivo Logger CSV
+        self.csv_filename = "data.csv"
+        self.csv_fieldnames = ["Calibracion","Presion","Temperatura","Sensor"]
+        
+    # MQTT message handler y actualizador del Loggers
+    def on_message(self, client, userdata, message):
+        #self.ui.lcdNumberMax.display(12)
+        payload_str = message.payload.decode("utf-8")
+        payload_str = payload_str.split(' | ') #Separa el string que viene con el caracter |
+        topic_str = message.topic
+        type(topic_str)
+        if(message.topic == self.subscribe_topic_1):
+            self.ui.lcdPresion1.display(payload_str[0])
+            self.ui.lcdTemperatura1.display(payload_str[2])
+        elif(message.topic == self.subscribe_topic_2):
+            self.ui.lcdPresion2.display(payload_str[0])
+            self.ui.lcdTemperatura2.display(payload_str[2])
+        with open(self.csv_filename, "a", newline="") as csv_file:
+            writer = csv.DictWriter(csv_file, fieldnames=self.csv_fieldnames)
+            writer.writerow({
+                "Calibracion": payload_str[1],
+                "Presion": payload_str[0],
+                "Temperatura": payload_str[2],
+                "Sensor": message.topic
+            })
+
+    # Conexión al broker MQTT
+    def on_connect(self,client, userdata, flags, rc):
+        print("Conectado al broker MQTT con código " + str(rc))
+        self.client.subscribe(self.subscribe_topic_1)
+        self.client.subscribe(self.subscribe_topic_2)
+
+    #Funcion para enviar cero para frenar
+    def sendStop(self):
+        self.client.publish(self.topic, 0)
+    
+    #Funcion para enviar epoch para sincronizar
+    def sendSincro(self):
+        self.ui.ei.text()
+        self.client.publish(self.topic, self.ui.ei.text())
+        #self.client.publish(self.topic, int(time.time()))
+
+    def startTimer(self):    
+        if self.ui.pushButton.text() == "Start Timer":
+            self.timer.start(1000) 
+            self.ui.pushButton.setText("Stop Timer")            
+        else:
+            self.ui.pushButton.setText("Start Timer")
+            self.timer.stop() 
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    frm = Form()
+    sys.exit(app.exec_())
